dbc0b7f94a689bb4f0545c7c06a65f6dddaff3b5
Glace97 <glacier.ali@gmail.com>
2021-11-13 23:34:52 +0100

    new game html page shows

--- src/com/company/Guess.java
+++ src/com/company/Guess.java
@@ -7,12 +7,12 @@ import java.util.Random;
 public class Guess {
     int numOfGuesses;
     int secretNum;
-    int clientID;
+    int sessionID;
     String index; //dynamically decide which page to show user
 
     public Guess(int cliendID) {
         this.secretNum = generateSecreteNum(); //"unique" for this session
-        this.clientID = cliendID;
+        this.sessionID = cliendID;
 
         //test HTML
         //NewGame
@@ -25,14 +25,23 @@ public class Guess {
                 "\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "    <form>\n" +
-                "        <input type=\"submit\">\n" +
-                "    </form>\n" +
+                "       <text>Welcome to the number guess game.Im thinking of a number between 1 and 100. What's your guess?</text>\n" +
+                "       <form method=\"post\" action=\"HTTPServer.java\">\n" +
+                "           <input type=\"text\" name=\"Guess\">\n" +
+                "       </form>\n" +
+                "       <form>\n" +
+                "       <input type=\"submit\" value=\"send\">\n" +
+                "       </form>\n" +
                 "</body>\n" +
                 "</html>";
 
         this.index = newGame;
     }
+
+    /*TODO:
+    *   1. en indexformat om man gissar fel --> boolean om man gissat för lågt/högt
+    *   2. indexformat om man gissar rätt --> ska printa ut numOfGuesses
+    * */
 
     public int generateSecreteNum() {
         Random random = new Random();
--- src/com/company/HTTPServer.java
+++ src/com/company/HTTPServer.java
@@ -10,12 +10,12 @@ public class HTTPServer {
     BufferedReader request;
     PrintWriter response;
     ArrayList<Guess> sessions;
-    static int clientID;
+    static int nextSessionID;
 
     public HTTPServer(int portNum) throws IOException {
         serverSocket = new ServerSocket(portNum);
         System.out.println("GuessingGame server up and running!");
-        clientID = 0;
+        nextSessionID = 1;
         sessions = new ArrayList<>();
     }
 
@@ -23,7 +23,7 @@ public class HTTPServer {
 
         // Wait for new clients to connect to the game
         // Naive approach, this does not scale and only handles one client a time
-        while(!serverSocket.isClosed()) {
+        while (!serverSocket.isClosed()) {
             System.out.println("Waiting for client...");
             Socket client = serverSocket.accept(); //client request
             System.out.println("Client connected.");
@@ -32,38 +32,80 @@ public class HTTPServer {
             request = new BufferedReader(new InputStreamReader(client.getInputStream()));
             response = new PrintWriter(client.getOutputStream());
 
-            String startLine = request.readLine();  // [<method> <target> <protocol version>]
 
-            //String startLine = "GET /background.png HTTP/1.0"; //example string for testing
-            String[] tokens = startLine.split(" ");
+            // Check if the client already has a session
+            // Extract the sessionID for session
+            String line;
+            String[] cookieHeader;
+            String[] keyVal;
+            int sessionID = 0;
+            while (!(line = request.readLine()).isEmpty()) {
+
+                if(line.contains("Guess:"))
+
+                if (line.contains("Cookie:")) {
+                    cookieHeader = line.split(" ");
+                    keyVal = cookieHeader[1].split("=");
+                    sessionID = Integer.parseInt(keyVal[1]);
+                }
+            }
+
+            //hämta gamelogik isf
+
 
             //TODO: skippa favicon requesten (den ska inte renderera någonting nytt;
 
-            //if new game
-            Guess session = new Guess(clientID);
-            sessions.add(session);
 
-            String index = session.index;
+
+            // might be overwritten with a existing session, might be a new one
+            Guess session;
 
             response.println("HTTP/1.1 200 OK");
-            response.print("Content-Type: index/html");
+            response.println("Content-Type: text/html");
             response.println("Content-Length: " + index.length());
-            //TODO: printa Set-Cookie headern
 
-            //TODO: printa strängar nödvändiga för gamelogiken
+            if (sessionID == 0) {
+                // The first session for the client. Set cookie with next available sessionID
+                response.println("Set-Cookie: sessionID=" + nextSessionID);
 
-            //TODO: printa form
-            response.println(index);
+                //Generate a new session
+                session = new Guess(nextSessionID);
+                sessions.add(session);
 
-            response.flush();
+                nextSessionID++;
+            } else {
+                // Assign the original cookie
+                response.println("Set-Cookie: sessionID=" + sessionID);
 
-            //close the connection
-            request.close();
-            response.close();
-            client.close();
+                //plocka ut gissningen härifån?
+                for (Guess session : sessions) {
+                    if (session.sessionID == sessionID) {
+                        //Vi har rätt session, kolla om vi har rätt gissnig
+
+
+
+                        String index = session.index;
+                    }
+                }
+            }
         }
 
+        response.println("\r\n");
+        //TODO: printa Set-Cookie headern
+
+        //TODO: printa strängar nödvändiga för gamelogiken
+
+        //TODO: printa form
+        response.println(index);
+        response.flush();
+
+        //close the connection
+        request.close();
+        response.close();
+        client.close();
     }
+
+}
 
     public static void main(String[] args) throws IOException {
         HTTPServer guessingGame = new HTTPServer(8080);
